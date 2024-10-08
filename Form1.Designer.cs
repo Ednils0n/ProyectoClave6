30e1e1b38cf1c9e48ad3f3d1781d2f93d5c8381d
Ednilson Oswaldo Montes Landos <ML23030@ti.ues.edu.sv>
2024-10-08 16:46:57 -0600

    ARREGLOS FORMS

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,13 +37,16 @@ namespace ProyectoClave6
             this.txtContra = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(321, 243);
+            this.btnSalir.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(471, 248);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(86, 46);
+            this.btnSalir.Size = new System.Drawing.Size(95, 46);
             this.btnSalir.TabIndex = 0;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -51,9 +54,10 @@ namespace ProyectoClave6
             // 
             // btnConectar
             // 
-            this.btnConectar.Location = new System.Drawing.Point(702, 303);
+            this.btnConectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.Location = new System.Drawing.Point(763, 302);
             this.btnConectar.Name = "btnConectar";
-            this.btnConectar.Size = new System.Drawing.Size(86, 46);
+            this.btnConectar.Size = new System.Drawing.Size(92, 46);
             this.btnConectar.TabIndex = 1;
             this.btnConectar.Text = "ConectarSQL";
             this.btnConectar.UseVisualStyleBackColor = true;
@@ -61,7 +65,8 @@ namespace ProyectoClave6
             // 
             // btnSiguienteForms
             // 
-            this.btnSiguienteForms.Location = new System.Drawing.Point(392, 180);
+            this.btnSiguienteForms.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSiguienteForms.Location = new System.Drawing.Point(379, 208);
             this.btnSiguienteForms.Name = "btnSiguienteForms";
             this.btnSiguienteForms.Size = new System.Drawing.Size(86, 46);
             this.btnSiguienteForms.TabIndex = 2;
@@ -72,22 +77,23 @@ namespace ProyectoClave6
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(206, 100);
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(278, 113);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.Size = new System.Drawing.Size(80, 14);
             this.label1.TabIndex = 3;
             this.label1.Text = "Inicia Sesion";
             // 
             // txtSesion
             // 
-            this.txtSesion.Location = new System.Drawing.Point(279, 97);
+            this.txtSesion.Location = new System.Drawing.Point(364, 107);
             this.txtSesion.Name = "txtSesion";
             this.txtSesion.Size = new System.Drawing.Size(168, 20);
             this.txtSesion.TabIndex = 4;
             // 
             // txtContra
             // 
-            this.txtContra.Location = new System.Drawing.Point(279, 135);
+            this.txtContra.Location = new System.Drawing.Point(364, 135);
             this.txtContra.Name = "txtContra";
             this.txtContra.Size = new System.Drawing.Size(168, 20);
             this.txtContra.TabIndex = 5;
@@ -95,27 +101,53 @@ namespace ProyectoClave6
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(208, 136);
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(284, 138);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.Size = new System.Drawing.Size(74, 14);
             this.label2.TabIndex = 6;
             this.label2.Text = "Contraseña";
             // 
             // btnIniciarSesion
             // 
-            this.btnIniciarSesion.Location = new System.Drawing.Point(229, 180);
+            this.btnIniciarSesion.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciarSesion.Location = new System.Drawing.Point(281, 248);
             this.btnIniciarSesion.Name = "btnIniciarSesion";
-            this.btnIniciarSesion.Size = new System.Drawing.Size(86, 46);
+            this.btnIniciarSesion.Size = new System.Drawing.Size(92, 46);
             this.btnIniciarSesion.TabIndex = 7;
             this.btnIniciarSesion.Text = "Iniciar Sesion";
             this.btnIniciarSesion.UseVisualStyleBackColor = true;
             this.btnIniciarSesion.Click += new System.EventHandler(this.btnIniciarSesion_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(276, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Work Office S.A. de C.V";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(259, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(331, 14);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Si usted no posee usuario y contraseña registrese Aqui.";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(867, 443);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnIniciarSesion);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtContra);
@@ -141,6 +173,8 @@ namespace ProyectoClave6
         private System.Windows.Forms.TextBox txtContra;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnIniciarSesion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -35,13 +35,16 @@ namespace ProyectoClave6
             this.label2 = new System.Windows.Forms.Label();
             this.txtContra = new System.Windows.Forms.TextBox();
             this.btnRegistrar1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnRegresar1
             // 
-            this.btnRegresar1.Location = new System.Drawing.Point(651, 259);
+            this.btnRegresar1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar1.Location = new System.Drawing.Point(334, 267);
             this.btnRegresar1.Name = "btnRegresar1";
-            this.btnRegresar1.Size = new System.Drawing.Size(82, 45);
+            this.btnRegresar1.Size = new System.Drawing.Size(111, 41);
             this.btnRegresar1.TabIndex = 0;
             this.btnRegresar1.Text = "Regresar";
             this.btnRegresar1.UseVisualStyleBackColor = true;
@@ -49,39 +52,42 @@ namespace ProyectoClave6
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(342, 119);
+            this.txtNombre.Location = new System.Drawing.Point(342, 115);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(147, 20);
+            this.txtNombre.Size = new System.Drawing.Size(155, 20);
             this.txtNombre.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(248, 122);
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(241, 119);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.Size = new System.Drawing.Size(95, 12);
             this.label1.TabIndex = 2;
             this.label1.Text = "Nombre Usuario";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(248, 154);
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(241, 154);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.Size = new System.Drawing.Size(70, 12);
             this.label2.TabIndex = 3;
             this.label2.Text = "Contraseña";
             // 
             // txtContra
             // 
-            this.txtContra.Location = new System.Drawing.Point(315, 151);
+            this.txtContra.Location = new System.Drawing.Point(315, 150);
             this.txtContra.Name = "txtContra";
-            this.txtContra.Size = new System.Drawing.Size(147, 20);
+            this.txtContra.Size = new System.Drawing.Size(174, 20);
             this.txtContra.TabIndex = 4;
             // 
             // btnRegistrar1
             // 
-            this.btnRegistrar1.Location = new System.Drawing.Point(315, 210);
+            this.btnRegistrar1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar1.Location = new System.Drawing.Point(334, 186);
             this.btnRegistrar1.Name = "btnRegistrar1";
             this.btnRegistrar1.Size = new System.Drawing.Size(111, 41);
             this.btnRegistrar1.TabIndex = 5;
@@ -89,11 +95,35 @@ namespace ProyectoClave6
             this.btnRegistrar1.UseVisualStyleBackColor = true;
             this.btnRegistrar1.Click += new System.EventHandler(this.btnRegistrar1_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(260, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Work Office S.A. de C.V";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(332, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Volver a la Interfaz:";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnRegistrar1);
             this.Controls.Add(this.txtContra);
             this.Controls.Add(this.label2);
@@ -115,5 +145,7 @@ namespace ProyectoClave6
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtContra;
         private System.Windows.Forms.Button btnRegistrar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
