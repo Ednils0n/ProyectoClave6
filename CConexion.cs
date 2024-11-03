b80d499f66546c2427aee98483ddef6f2cf0bb70
Ednilson Oswaldo Montes Landos <ML23030@ti.ues.edu.sv>
2024-11-02 22:02:49 -0600

    f

--- CConexion.cs
+++ CConexion.cs
@@ -8,28 +8,28 @@ using MySql.Data.MySqlClient;
 
 namespace ProyectoClave6
 {
-    // Clase CConexion que hereda de Usuario
+    //Clase CConexion que hereda de Usuario
     class CConexion : Usuario
     {
         private MySqlConnection conex = new MySqlConnection();
 
-        // Propiedades estáticas para la conexión
+        //Propiedades estáticas para la conexión
         static string servidor = "localhost";
         static string bd = "Proyecto6";
         static string usuario = "root";
         static string password = "ednilson";
         static string puerto = "3306";
 
-        // Cadena de conexión
+        //Cadena de conexión
         static string cadenaConexion = "server=" + servidor + ";" + "port=" + puerto + ";" +
             "user id=" + usuario + ";" + "password=" + password + ";" + "database=" + bd + ";";
 
-        // Constructor de la clase CConexion que llama al constructor de Usuario
+        //Constructor de la clase CConexion que llama al constructor de Usuario
         public CConexion(string nombreUsuario, string contrasena) : base(nombreUsuario, contrasena)
         {
         }
 
-        // Método para establecer la conexión
+        //Método para establecer la conexión
         public MySqlConnection EstablecerConexion()
         {
             try
